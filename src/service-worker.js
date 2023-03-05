815232d35e7ba5b1a08453f657a1d4c127cb3329
tensaye-o <yyuan.tech@gmail.com>
2023-03-05 11:16:33 +0900

    :bug: fix pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,10 +10,12 @@ async function activate() {
   await Promise.all(keys.map((key) => key !== version && caches.delete(key)))
 }
 
+async function fetching(e) {
+  const req = e.request
+  const res = await caches.match(req)
+  return res || fetch(req)
+}
+
 addEventListener('install', (e) => e.waitUntil(install()))
-addEventListener('fetch', (e) =>
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  )
-)
+addEventListener('fetch', (e) => e.respondWith(fetching(e)))
 addEventListener('activate', (e) => e.waitUntil(activate()))
